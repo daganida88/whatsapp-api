45a1636d8495f25e25a20271bfb0d711628bea69
Dagan <idan.dagan@imagen-ai.com>
2025-11-15 20:58:18 +0200

    Disabling renderers

--- src/index.js
+++ src/index.js
@@ -44,6 +44,16 @@ const clientConfig = {
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--no-first-run',
+            '--disable-extensions',
+            '--disable-background-timer-throttling',
+            '--disable-backgrounding-occluded-windows',
+            '--disable-renderer-backgrounding',
+            '--disable-features=TranslateUI',
+            '--disable-web-security',
+            '--disable-features=VizDisplayCompositor',
             '--user-data-dir=/app/session_data/session',
         ],
         executablePath: process.env.CHROME_BIN || '/usr/bin/chromium-browser'
